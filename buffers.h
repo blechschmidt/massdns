d9e9ec16888f2474b15f49c98a90ac6c0108b5cd
B. Blechschmidt <blechschmidt@users.noreply.github.com>
2016-06-26 02:03:54 +0200

    Remove unused file containing misleading security comment

--- buffers.h
+++ buffers.h
@@ -11,30 +11,4 @@ typedef struct buffer
     size_t len;
 } buffer_t;
 
-typedef struct stream_buffer
-{
-    void *data;
-    size_t offset;
-    size_t len;
-    size_t capacity;
-} stream_buffer_t;
-
-void stream_buffer_free(stream_buffer_t *buf);
-
-size_t stream_buffer_get_bytes_available(stream_buffer_t *buf);
-
-void stream_buffer_print_ascii(stream_buffer_t *buf);
-
-void stream_buffer_print(stream_buffer_t *buf);
-
-void stream_buffer_has_read(stream_buffer_t *buf, size_t len);
-
-void stream_buffer_has_appended(stream_buffer_t *buf, size_t len);
-
-int stream_buffer_ensure_increase(stream_buffer_t *buf, size_t len);
-
-void *stream_buffer_get_ptr(stream_buffer_t *buf);
-
-void stream_buffer_init(stream_buffer_t *buf, size_t capacity);
-
 #endif
