eef58d6ad9c67d8970ac1bd4e6916a5c14eff286
Quirin Scheitle <scheitle@net.in.tum.de>
2017-04-10 13:53:57 +1000

    include timestamp in log, place logfile in /tmp

--- main.c
+++ main.c
@@ -443,20 +443,17 @@ void massdns_handle_packet(ldns_pkt *packet, struct sockaddr_storage ns, void *c
     {
         if (LDNS_STATUS_OK != output_packet(buf, packet, ns, context))
         {
-            // fprintf(stderr, "ABORT: output packet status not OK \n");
-            // fprintf(stdout, "ABORT: output packet status not OK \n");
             ldns_buffer_free(buf);
-            fprintf(stderr, "CRITICAL: output packet status not OK for domain %s \n", lookup->domain);
-            fprintf(stdout, "CRITICAL: output packet status not OK for domain %s \n", lookup->domain);
-            
-            // log the offending domain
-            FILE *f = fopen("uncaught_formerr.txt", "a");
+            fprintf(stderr, "CRITICAL: output packet status not OK for domain %s -- check /tmp/massdns_uncaught_formerr.txt \n", lookup->domain);
+            fprintf(stdout, "CRITICAL: output packet status not OK for domain %s -- check /tmp/massdns_uncaught_formerr.txt \n", lookup->domain);
+
+            // log the offending domain to /tmp, as massdns does user privilege drop
+            FILE *f = fopen("/tmp/massdns_uncaught_formerr.txt", "a");
             if (f == NULL)
             {
-                printf("Error opening uncaught_formerr.txt!\n");
-                exit(1);
+                printf("Error opening /tmp/massdns_uncaught_formerr.txt!\n");
             }
-            fprintf(f, "%s\n", lookup->domain);
+            fprintf(f, "%d,%s\n", (int)now.tv_sec, lookup->domain);
             fclose(f);
 
             stats.formerr++;
