18fc7f616f930d5ea3fdd71d22b1ce4c20933730
B. Blechschmidt <github@blechschmidt.pw>
2021-09-23 02:06:19 +0200

    Automatically detect IP support for auth-addrs script

--- scripts/auth-addrs.sh
+++ scripts/auth-addrs.sh
@@ -8,11 +8,14 @@ fi
 NS=$(dig +short -t NS "$1")
 RESULT=""
 while read -r server; do
-	if [[ "$2" -ne "6" ]]; then
+	HAS_IPV4=$(ip -4 route | grep -q ^default; echo $?)
+	HAS_IPV6=$(ip -6 route | grep -q ^default; echo $?)
+
+	if [ "$2" != "6" ] || [ "$2" == "" ] && [ "$HAS_IPV4" == "0" ]; then
 		IP4=$(dig +short -t A "$server")
 		RESULT="$RESULT""$IP4"$'\n'
 	fi
-	if [[ "$2" -ne "4" ]]; then
+	if [ "$2" != "4" ] || [ "$2" == "" ] && [ "$HAS_IPV6" == "0" ]; then
                 IP6=$(dig +short -t AAAA "$server")
                 RESULT="$RESULT""$IP6"$'\n'
         fi
