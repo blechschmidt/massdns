125f29c2e80272bb876d4faff61dbaf460432abe
B. Blechschmidt <github@blechschmidt.pw>
2023-01-08 17:29:38 +0100

    Add wildcard handling to reconnaissance script

--- scripts/recon.py
+++ scripts/recon.py
@@ -4,7 +4,20 @@
 Authoritative Subdomain Enumeration Tool.
 
 This script performs subdomain enumeration against authoritative name servers directly and thus does not require third-
-party resolvers. The concurrency is determined automatically by massdns.
+party resolvers. The concurrency is determined automatically by massdns and supports hundreds of thousands queries per
+second, while delivering reliable results.
+
+Limitation: Zone delegation is only handled up to the delegation point. For example, if example.org is enumerated and
+sub.example.org is delegated to another name server, abc.sub.example.org will not be found by this script if "abc.sub"
+is contained in the word list. However, this script will report this fact as ?.sub.example.org in this case.
+
+This script performs differential testing. It will therefore not only report those subdomains for which a DNS record of
+the specified type (--type; A record by default) is found, but also include non-empty terminals or names for which
+another record type might exist.
+
+By default, the script performs wildcard detection in a second name resolution process. For example, if the script
+detects the presence of abc.sub.example.org and xyz.sub.example.org, it will query <random_string>.sub.example.org and
+compare the resulting records to those of abc.sub.example.org and xyz.sub.example.org.
 """
 
 import argparse
@@ -15,10 +28,12 @@ import os.path
 import random
 import re
 import shutil
+import socket
 import string
 import subprocess
 import sys
 import tempfile
+from collections import OrderedDict
 from typing import Optional
 
 import dns.asyncresolver
@@ -29,8 +44,33 @@ TEMP_PREFIX = 'massdns'
 tempdir = None
 
 
+def ip_supported(version):
+    try:
+        from pyroute2.iproute import IPRoute
+    except ImportError:
+        return version == 4
+    family = socket.AF_INET if version == 4 else socket.AF_INET6
+    with IPRoute() as ip:
+        routes = ip.route('dump', family=family)
+        for route in routes:
+            if route.get('dst_len') == 0 and route.get('table') == 254:
+                return True
+    return False
+
+
 def random_subdomain(length=10):
     return ''.join([random.choice(string.ascii_lowercase) for _ in range(length)])
+
+
+def is_descendant(name, parent, real_descendant=False):
+    if isinstance(name, str):
+        name = canonicalize(name)
+    if isinstance(parent, str):
+        parent = canonicalize(parent)
+    required_length = len(parent)
+    if real_descendant:
+        required_length += 1
+    return len(name) >= required_length and name[:len(parent)] == parent
 
 
 def extract_record_data(answer):
@@ -68,8 +108,12 @@ async def get_ips(names, resolver=None):
         resolver = dns.asyncresolver.Resolver()
 
     awaitables = []
+
     for name in names:
-        awaitables.append(resolver.resolve(name, dns.rdatatype.A))
+        if ip_supported(4):
+            awaitables.append(resolver.resolve(name, dns.rdatatype.A))
+        if ip_supported(6):
+            awaitables.append(resolver.resolve(name, dns.rdatatype.AAAA))
 
     result = []
     for answer in await asyncio.gather(*awaitables):
@@ -89,7 +133,7 @@ def massdns_find_path():
 
 
 def create_rrset(section):
-    return set(frozenset((k, v) for k, v in x.items() if k != 'name') for x in section)
+    return set(frozenset((k, v) for k, v in x.items() if k != 'name' and k != 'ttl') for x in section)
 
 
 async def working_servers(qname, nameserver_ips):
@@ -116,15 +160,24 @@ class MultiFileLineReader:
         self.handles = []
 
     def __iter__(self):
-        self.iterators = [h.__iter__() for h in self.handles]
+        self.iterators = [iter(h) for h in self.handles]
         self.file = 0
         return self
 
     def __next__(self):
-        line = self.iterators[self.file].__next__()
-        self.file += 1
-        if self.file >= len(self.iterators):
-            self.file = 0
+        while True:
+            it = self.iterators[self.file]
+            try:
+                line = next(it)
+                break
+            except StopIteration:
+                self.iterators.remove(it)
+                if len(self.iterators) == 0:
+                    raise StopIteration
+            finally:
+                self.file += 1
+                if self.file >= len(self.iterators):
+                    self.file = 0
         return line
 
     def __enter__(self):
@@ -140,6 +193,108 @@ class DnsServerBehavior:
     global_noerror: Optional[bool] = None  # Whether the server returns NOERROR for non-existing names
 
 
+class DnsNode:
+    def __init__(self):
+        self.children = {}
+        self.hits = 0
+        self.data = None
+        self.delegation = False
+
+    def add(self, name):
+        self.hits += 1
+        if isinstance(name, str):
+            name = canonicalize(name)
+            it = iter(name)
+        elif isinstance(name, list):
+            it = iter(name)
+        else:
+            it = name
+        try:
+            first_label = next(it)
+        except StopIteration:
+            return self
+        if first_label not in self.children:
+            self.children[first_label] = DnsNode()
+        return self.children[first_label].add(it)
+
+    def remove(self, name):
+        if isinstance(name, str):
+            name = canonicalize(name)
+        parent = self.find(name[:-1])
+        if parent is None:
+            return False
+        del parent.children[name[-1]]
+        return True
+
+    def find(self, name):
+        if isinstance(name, str):
+            name = canonicalize(name)
+            it = iter(name)
+        elif isinstance(name, list):
+            it = iter(name)
+        else:
+            it = name
+        try:
+            first_label = next(it)
+        except StopIteration:
+            return self
+        if first_label not in self.children:
+            return None
+        return self.children[first_label].find(it)
+
+    def traverse(self):
+        return self._traverse([])
+
+    def _traverse(self, name):
+        for child in self.children:
+            new_name = name + [child]
+            yield new_name, self.children[child]
+            for c in self.children[child]._traverse(new_name):
+                yield c
+
+    def sort(self):
+        for name, node in self.traverse():
+            node.children = OrderedDict((key, node.children[key]) for key in sorted(node.children.keys()))
+
+
+def massdns_show_progress(proc, total):
+    last = 0
+    processed = 0
+    with tqdm.tqdm(total=total) as pbar:
+        for line in proc.stderr:
+            decoded = line.decode()
+            try:
+                loaded = json.loads(decoded)
+                processed = loaded['processed_queries']
+            except json.JSONDecodeError:
+                sys.stdout.write(decoded)
+            pbar.update(processed - last)
+            last = processed
+        pbar.update(total - processed)
+
+    proc.wait()
+
+
+def log(msg):
+    sys.stderr.write(msg + '\n')
+
+
+def canonicalize(name):
+    return list(reversed(name.rstrip('.').split('.')))
+
+
+def get_auth_server(auth, soa):
+    soa = canonicalize(soa)
+    result = None
+    for record in auth:
+        if record['type'] != 'NS':
+            continue
+        name = canonicalize(record['name'])
+        if is_descendant(name, soa) and (result is None or len(name) < len(result)):
+            result = name
+    return result
+
+
 async def main():
     global tempdir
 
@@ -150,16 +305,19 @@ async def main():
     parser = argparse.ArgumentParser(description='Authoritative Subdomain Enumeration Tool.')
     parser.add_argument('--domain', '-d', metavar='DOMAIN', help='Domain name.', required=True)
     parser.add_argument('--wordlist', '-w', metavar='WORDLIST', help='Subdomain wordlist file.', required=True)
+    parser.add_argument('--type', '-t', help='DNS record type to test against.', default='A')
     parser.add_argument('--skip-avail-check', help='Do not test whether nameservers are available.',
                         action='store_true')
     parser.add_argument('--no-wildcard-filter', help='Do not filter replies that appear to be wildcard responses.',
                         action='store_true')
+    parser.add_argument('--wildcard-threshold', help='Wildcard check threshold.', type=int, default=2)
     parser.add_argument('--massdns', help='Path to massdns binary.')
     args = parser.parse_args()
-    domain = args.domain.encode('idna').decode('ascii')
+    domain = args.domain.encode('idna').decode('ascii').rstrip('.')
+    domain_labels = domain.split('.')
 
     if not re.match(r'^[a-zA-Z0-9_-][a-zA-Z0-9_.-]+$', domain):
-        sys.stderr.write('Invalid domain.\n')
+        log('Invalid domain.')
         sys.exit(1)
 
     massdns_path = massdns_find_path() if args.massdns is None else args.massdns
@@ -176,25 +334,27 @@ async def main():
 
     with open(resolvers_path, 'w') as resolvers_file:
         soa = await get_soa(domain)
-        sys.stderr.write('SOA for %s: %s\n' % (domain, soa))
+        log('SOA for %s: %s' % (domain, soa))
         nameservers = list(map(lambda ns: ns.rstrip('.'), await get_ns_names(soa)))
-        sys.stderr.write(domain + ' nameservers: %s\n' % ', '.join(nameservers))
+        log(domain + ' nameservers: %s' % ', '.join(nameservers))
         nameserver_ips = await get_ips(nameservers)
-        sys.stderr.write(domain + ' nameserver IP addresses: %s\n' % ', '.join(nameserver_ips))
+        log(domain + ' nameserver IP addresses: %s' % ', '.join(nameserver_ips))
 
         if not args.skip_avail_check:
             working_ns = set(await working_servers(domain, nameserver_ips))
             non_working = set(nameserver_ips) - working_ns
             if len(non_working) == 0:
-                sys.stderr.write('All nameservers are up.\n')
+                log('All nameservers are up.')
             else:
-                sys.stderr.write('Some nameservers are down: %s\n' % ', '.join(non_working))
+                log('Some nameservers are down: %s' % ', '.join(non_working))
             nameserver_ips = list(working_ns)
 
         resolver_file_contents = '\n'.join(nameserver_ips)
         resolvers_file.write(resolver_file_contents)
 
+    log('Writing enumeration input file ...')
     wildcard_test_name = (random_subdomain() + '.' + domain).lower()
+    total = 1
     with open(wordlist_path) as wordlist_file, open(permutations_path, 'w') as permutations_file:
         permutations_file.write(wildcard_test_name + '\n')
         for line in wordlist_file:
@@ -204,6 +364,7 @@ async def main():
             if not subdomain.endswith('.'):
                 subdomain += '.'
             permutations_file.write((subdomain + domain).lower() + '\n')
+            total += 1
 
     resolver = dns.asyncresolver.Resolver()
     resolver.nameservers = nameserver_ips
@@ -218,52 +379,43 @@ async def main():
     except dns.resolver.NXDOMAIN:
         server_behavior.global_noerror = False
     predicate = 'returns' if server_behavior.global_noerror else 'does not return'
-    sys.stderr.write('The nameserver ' + predicate + ' NOERROR for non-existing domains.\n')
+    log('The nameserver ' + predicate + ' NOERROR for non-existing domains.')
+
+    log('MassDNS enumeration ...')
 
     output_flags = ['Je', '--filter', 'NOERROR'] if not server_behavior.global_noerror else ['Je']
     proc = subprocess.Popen([massdns_path, '-o', *output_flags, '-s', 'auto', '--retry', 'never', '-r', resolvers_path,
                              '-w', output_path, '--status-format', 'json', '--processes', str(cpu_count),
+                             '-t', args.type,
                              permutations_path], stderr=subprocess.PIPE)
-    last = 0
-    with tqdm.tqdm(total=100) as pbar:
-        for line in proc.stderr:
-            decoded = line.decode()
-            try:
-                percent = json.loads(decoded)['progress']['percent']
-            except json.JSONDecodeError:
-                sys.stdout.write(decoded)
-            pbar.update(percent - last)
-            last = percent
 
-    proc.wait()
+    massdns_show_progress(proc, total)
 
     def filter_answers(_):
         return False
 
-    def filter_authorities(_):
-        return False
+    log('Output processing ...')
 
     errors_seen = False
-    # TODO: Improve performance by piping stdout and returning matching results immediately instead of using a temp file
     with MultiFileLineReader([output_path + str(i) for i in range(0, cpu_count)]) as f:
         if not args.no_wildcard_filter and server_behavior.global_noerror:
             for line in f:
                 parsed = json.loads(line)
                 if parsed['name'].rstrip('.') == wildcard_test_name.rstrip('.'):
                     answers = parsed.get('data', {}).get('answers', [])
-                    authorities = parsed.get('data', {}).get('authorities', [])
 
                     def func(compare):
-                        def filter(section):
-                            cmp = set(frozenset((k, v) for k, v in x.items() if k != 'name') for x in section)
+                        def f(section):
+                            cmp = create_rrset(section)
                             return compare == cmp
 
-                        return filter
+                        return f
 
                     filter_answers = func(create_rrset(answers))
-                    filter_authorities = func(create_rrset(authorities))
 
                     break
+
+    dns_tree = DnsNode()
 
     with MultiFileLineReader([output_path + str(i) for i in range(0, cpu_count)]) as f:
         for line in f:
@@ -275,14 +427,86 @@ async def main():
                 authorities = data.get('authorities', [])
                 answers = data.get('answers', [])
 
-                if filter_answers(answers) and filter_authorities(authorities):
+                if filter_answers(answers):
                     continue
-                print(parsed['name'].rstrip('.'))
+
+                # In the case of zone delegation, we mark the delegated node in the DNS tree
+                if 'aa' not in parsed['flags'] and parsed['status'] == 'NOERROR':
+                    auth_server = get_auth_server(authorities, soa)
+                    if auth_server is not None and is_descendant(parsed['name'], auth_server):
+                        added = dns_tree.add(auth_server)
+                        added.data = parsed
+                        added.delegation = True
+                        continue
+
+                added = dns_tree.add(parsed['name'])
+                added.data = parsed
             elif 'error' in parsed:
                 if not errors_seen:
                     errors_seen = True
                     sys.stderr.write('Resolving the following names failed:\n')
                 sys.stderr.write(parsed['name'].rstrip('.') + '\n')
+
+    if not args.no_wildcard_filter:
+        log('Writing wildcard test input file ...')
+
+        wildcard_subdomain1 = random_subdomain()
+        wildcard_check = DnsNode()
+        wildcard_in_path = os.path.join(tempdir, 'wildcard_in')
+        total = 0
+        with open(wildcard_in_path, 'w') as wildcard_file:
+            for name, node in dns_tree.traverse():
+                if len(name) <= len(domain_labels):
+                    continue
+                if node.hits > args.wildcard_threshold and not node.delegation:
+                    normalized = '.'.join(reversed(name))
+                    wildcard_check.add(normalized).data = node.data
+                    wildcard_file.write(wildcard_subdomain1 + '.' + normalized + '\n')
+                    total += 1
+
+        wildcard_out_path = os.path.join(tempdir, 'wildcard_out')
+
+        proc = subprocess.Popen([massdns_path, '-o', 'Je', '-s', 'auto', '--retry', 'never', '-r', resolvers_path,
+                                 '-w', wildcard_out_path, '--status-format', 'json', '--processes', str(cpu_count),
+                                 '-t', args.type,
+                                 wildcard_in_path], stderr=subprocess.PIPE)
+
+        massdns_show_progress(proc, total)
+
+        with MultiFileLineReader([wildcard_out_path + str(i) for i in range(0, cpu_count)]) as f:
+            for line in f:
+                parsed = json.loads(line)
+                answers = create_rrset(parsed.get('data', {}).get('answers', []))
+                name = canonicalize(parsed['name'])[:-1]
+
+                node = dns_tree.find(name)
+                if node is None:
+                    continue
+                erase = []
+                for subdomain, child in node.traverse():
+                    if child.data is None:
+                        continue
+                    answers_cmp = create_rrset(child.data.get('data', {}).get('answers', []))
+                    if answers == answers_cmp:
+                        erase.append(subdomain)
+                for n in erase:
+                    node.remove(n)
+
+                if parsed['status'] == 'NOERROR':
+                    child = DnsNode()
+                    child.data = parsed
+                    node.children = {'*': child}
+
+    dns_tree.sort()
+
+    for name, node in dns_tree.traverse():
+        if len(name) <= len(domain_labels):
+            continue
+        print_name = '.'.join(reversed(name))
+        if node.delegation:
+            print('?.' + print_name)
+            continue
+        print(print_name)
 
 
 asyncio.run(main())
